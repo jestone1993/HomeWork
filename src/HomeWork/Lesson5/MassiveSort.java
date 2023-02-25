77d8fdec174661ff660700697208b69982ed18d0
jestone1993 <lakewood35801@gmail.com>
2023-02-25 15:47:10 +0300

    Add Lesson5

--- src/HomeWork/Lesson5/MassiveSort.java
+++ src/HomeWork/Lesson5/MassiveSort.java
@@ -7,13 +7,9 @@ public class MassiveSort {
         int arr[][] = {{1, 3, 40, 90, 7},
                 {6, 88, 5, 23}};
         for (int i = 0; i < arr.length; i++) {
-            for (int k = 0; k < arr[i].length; k++) {
-                Arrays.sort(arr[i]);
-
-            }
+            Arrays.sort(arr[i]);
             System.out.printf(Arrays.toString(arr[i]));
         }
-
-
     }
+
 }
