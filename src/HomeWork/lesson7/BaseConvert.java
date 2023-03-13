66ea7f0eb663785565f7846a281fa2cc8ac8ba56
jestone1993 <lakewood35801@gmail.com>
2023-03-13 07:14:56 +0300

    Add Lesson7

--- src/HomeWork/lesson7/BaseConvert.java
+++ src/HomeWork/lesson7/BaseConvert.java
@@ -2,26 +2,12 @@ package HomeWork.lesson7;
 
 public class BaseConvert {
     public static void main(String[] args) {
-        System.out.println("Кельвин " + convert());
-        System.out.println("Фаренгейт " + convert1());
+        System.out.println("Кельвин " + Convert.convertKelvin());
+        System.out.println("Фаренгейт " + Convert.convertFahrenheit());
 
 
     }
 
-
-    public static double convert() {
-        double celsium = 20;
-        double tK = (celsium + 273.15);
-        return tK;
-
-
-    }
-
-    public static double convert1() {
-        double celsium = 20;
-        double tF = 1.8 * celsium + 32;
-        return tF;
-    }
 
 }
 
--- src/HomeWork/lesson7/Main.java
+++ src/HomeWork/lesson7/Main.java
@@ -5,47 +5,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Main monday = new Main(Day.MONDAY);
-        monday.Examination();
-        Main tuesday = new Main(Day.TUESDAY);
-        tuesday.Examination();
-        Main wednesday = new Main(Day.WEDNESDAY);
-        wednesday.Examination();
-        Main thursday = new Main(Day.THURSDAY);
-        thursday.Examination();
-        Main friday = new Main(Day.FRIDAY);
-        friday.Examination();
-        Main saturday = new Main(Day.SATURDAY);
-        saturday.Examination();
-        Main sunday = new Main(Day.SUNDAY);
-        sunday.Examination();
-        String name = "MONDAY";
-      /*  Day day1 = Day.valueOf(name);
-       System.out.println(Day.valueOf(name));
-        System.out.println(Arrays.toString(Day.values()));
-*/
+        Day[] days = Day.values();
+
+
+
+
+        for (Day day : days)
+        {
+            System.out.println(day);
+            if (day == Day.SUNDAY || day == Day.SATURDAY)
+            {
+                System.out.println("Выходной");
+            }
+            else
+            {
+                System.out.println("Рабочий");
+            }
     }
-
-    Day day;
-
-    public Main(Day day) {
-        this.day = day;
     }
-
-    public void Examination() {
-        switch (day) {
-            case MONDAY -> System.out.println(Day.MONDAY + " " + "Рабочий день");
-            case TUESDAY -> System.out.println(Day.TUESDAY + " " + "Рабочий день");
-            case WEDNESDAY -> System.out.println(Day.WEDNESDAY + " " + "Рабочий день");
-            case THURSDAY -> System.out.println(Day.THURSDAY + " " + "Рабочий день");
-            case FRIDAY -> System.out.println(Day.FRIDAY + " " + "Рабочий день");
-            case SATURDAY -> System.out.println(Day.SATURDAY + " " + "Выходной день");
-            case SUNDAY -> System.out.println(Day.SUNDAY + " " + "Выходной день");
-        }
-
-    }
-
-
 }
 
 
