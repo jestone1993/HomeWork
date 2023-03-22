27e14e9bd2ab0f75a1764f9314aecffd5f5e08b6
jestone1993 <lakewood35801@gmail.com>
2023-03-22 17:15:14 +0300

    Add Lesson10 Exercise1

---
+++ src/HomeWork/lesson10/Exercise4/StringMethod.java
@@ -0,0 +1,52 @@
+package HomeWork.lesson10.Exercise4;
+
+import java.sql.SQLOutput;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class StringMethod {
+    public static void firstChars(String str) {
+        char[] chars = new char[9];
+        str.getChars(0, 5, chars, 0);
+        str.getChars(9, 13, chars, 5);
+        System.out.print("1)");
+        System.out.println(chars);
+    }
+
+    public static void letters(String str) {
+        String str1 = str.join("/", "qwe", "ere", "x", "p").replace("qwe", "yyy").replace("ere", "yyy").replace("x", "y").replace("p", "y").toUpperCase();
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println("4)Letters: " + stringBuilder.append(str1));
+    }
+
+    public static void onlyWordsY(String str) {
+        String str1 = str.join("/", "qwe", "ere", "x", "p").replace("qwe", "yyy").replace("ere", "yyy").replace("x", "y").replace("p", "y");
+        System.out.println("3)" + str1);
+
+    }
+
+    public static void replaceSubString1(String str) {
+        String str1 = str.replace("qwe", "***");
+        String str2 = str.replace("ere", "***");
+        String str3 = str.replace("1x1p", "1*1*");
+        str2.split("qwe", 3);
+        System.out.println("2)" + str1.substring(0, 9) + str2.substring(9, 19) + str3.substring(19));
+    }
+
+    public static void Search(String str) {
+        System.out.println(str.toLowerCase().contains("abc"));
+
+
+    }
+
+    public static void CheckStartWith(String str) {
+        System.out.println(str.startsWith("555", 0));
+
+    }
+
+    public static void CheckLastWith(String str) {
+        System.out.println(str.endsWith("1a2b"));
+    }
+}
+
